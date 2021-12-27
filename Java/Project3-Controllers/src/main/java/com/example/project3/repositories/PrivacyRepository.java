2d2dfe5e8d87bd9c252da863929104d11939b743
wjbechtle <william.bechtle@revature.net>
2021-12-27 11:23:21 -0500

    Integrated controllers with entities

--- Java/Project3-Controllers/src/main/java/com/example/project3/repositories/PrivacyRepository.java
+++ Java/Project3-Controllers/src/main/java/com/example/project3/repositories/PrivacyRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PrivacyRepository extends JpaRepository<Privacy, Long>{
+public interface PrivacyRepository extends JpaRepository<Privacy, Long> {
+    Privacy findByprivacyid(Long id);
 }
