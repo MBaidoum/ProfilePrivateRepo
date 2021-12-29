0277cd3bf777d624fffbfe893fec75e1698b1c94
jlavigneRev <james.lavigne@revature.net>
2021-12-29 17:15:05 -0500

    Added profile pic uploading to controllers
    Added AWS S3 SDK dependency to pom, added controller method for uploading pictures, added service method to handle upload logic and set link in db.

---
+++ Java/Project3-Controllers/src/main/java/com/example/project3/services/PrivacyService.java
@@ -0,0 +1,13 @@
+package com.example.project3.services;
+
+import com.example.project3.entities.Privacy;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Service
+public interface PrivacyService {
+    public List<Privacy> getAllPrivacy();
+    public Privacy getPrivacyById(long id);
+}
